b98fc1cef05b509626fae796f87d214238a67f76
Herencia López-Menchero, Andrés <ANDRES.HERENCIA.CONTRACTOR@BBVA.COM>
2025-04-20 14:35:11 +0200

    add: O(n^2) version of the countFairPairs algorithm

--- medium/countFairPairs.py
+++ medium/countFairPairs.py
@@ -12,7 +12,7 @@ class Solution(object):
         self.upper = upper
 
         result = list()
-        cont = 1
+        cont = 0
         for num in self.nums:
             number = [num] * cont
             aux = list(zip(number, self.nums[0:cont]))
@@ -21,8 +21,7 @@ class Solution(object):
                 tup = (elem[0], elem[1]) if elem[0] < elem[1] else (elem[1], elem[0])
                 if sum >= self.lower and sum <= self.upper:
                     result.append(tup) 
-                if elem == aux[-1]:
-                    cont += 1
+            cont += 1
         return len(result)
     
 sol = Solution()
@@ -30,3 +29,5 @@ nums = [0,1,7,4,4,5]; lower = 3; upper = 6
 print(sol.countFairPairs(nums, lower, upper) == 6)
 nums = [1,7,9,2,5]; lower = 11; upper = 11
 print(sol.countFairPairs(nums, lower, upper) == 1)
+nums = [0,0,0,0,0,0]; lower = 0; upper = 0
+print(sol.countFairPairs(nums, lower, upper) == 15)
