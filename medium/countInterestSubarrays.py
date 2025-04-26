8a698d3f06425daa2c2b7da6aafb8adac9134b81
Herencia López-Menchero, Andrés <ANDRES.HERENCIA.CONTRACTOR@BBVA.COM>
2025-04-26 13:52:19 +0200

    feat: add countInterestSubarrays problem

---
+++ medium/countInterestSubarrays.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
+        ws = 1
+        aux = 0
+        while ws <= (len(nums)):
+            for n in range(len(nums) - ws + 1):
+                subarray = nums[n:n+ws]
+                cnt = 0
+                for i in range(len(subarray)):
+                    if subarray[i] % modulo == k:
+                        cnt += 1
+                if cnt % modulo == k:
+                        cnt = 0; aux += 1
+            ws += 1
+        return aux     
+
+sol = Solution()
+print(sol.countInterestingSubarrays([3,2,4], modulo = 2, k = 1))
+print(sol.countInterestingSubarrays([3,1,9,6], modulo = 3, k = 0))
--- medium/test.py
+++
@@ -1,5 +0,0 @@
-a = [1, 2, 3, 4, 5]
-b = [1] * len(a)
-
-for x, y in zip(b, a):
-    print((x, y))
